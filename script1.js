9d5529dfed6d559182dfeee32df1bf0d54d011e7
Gokul-96 <gokulakrishnan.m96@gmail.com>
2023-06-01 00:18:19 +0530

    check

---
+++ script1.js
@@ -0,0 +1,47 @@
+setTimeout(()=>{
+    let countDown = document.getElementsByClassName("countDown");
+    countDown[0].textContent = "10";
+    setTimeout(()=>{
+        let countDown = document.getElementsByClassName("countDown");
+        countDown[0].textContent = "9";
+        setTimeout(()=>{
+            let countDown = document.getElementsByClassName("countDown");
+            countDown[0].textContent = "8";
+            setTimeout(()=>{
+                let countDown = document.getElementsByClassName("countDown");
+                countDown[0].textContent = "7";
+                setTimeout(()=>{
+                    let countDown = document.getElementsByClassName("countDown");
+                    countDown[0].textContent = "6";
+                    setTimeout(()=>{
+                        let countDown = document.getElementsByClassName("countDown");
+                        countDown[0].textContent = "5";
+                        setTimeout(()=>{
+                            let countDown = document.getElementsByClassName("countDown");
+                            countDown[0].textContent = "4";
+                            setTimeout(()=>{
+                                let countDown = document.getElementsByClassName("countDown");
+                                countDown[0].textContent = "3";
+                                setTimeout(()=>{
+                                    let countDown = document.getElementsByClassName("countDown");
+                                    countDown[0].textContent = "2";
+                                    setTimeout(()=>{
+                                        let countDown = document.getElementsByClassName("countDown");
+                                        countDown[0].textContent = "1";
+                                        setTimeout(()=>{
+                                            let countDown = document.getElementsByClassName("countDown");
+                                            countDown[0].textContent = "Happy Independence Day";
+                                            document.body.style.backgroundColor = 'yellow'
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
+
+},1000)
